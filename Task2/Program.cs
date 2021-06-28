8fb606e5ff73e9faf7ea6db3abdd2664eb6b7481
milanfilipovicnp <milanfilipovicnp@hotmail.com>
2021-06-28 18:05:10 +0200

    first commit

---
+++ Task2/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {/* Start with the empty list */
+                LinkedList llist = new LinkedList();
+                llist.push(1);
+                llist.push(3);
+                llist.push(1);
+                llist.push(2);
+                llist.push(1);
+
+                Console.WriteLine("Count of nodes is " +
+                                llist.getCount());
+
+            }
+        }
+        public class Node
+        {
+            public int data;
+            public Node next;
+            public Node(int d)
+            {
+                data = d; next = null;
+            }
+        }
+        // Linked List class
+        public class LinkedList
+        {
+            Node head; // head of list
+
+            /* Inserts a new Node at front of the list. */
+            public void push(int new_data)
+            {
+                /* 1 & 2: Allocate the Node &
+                        Put in the data*/
+                Node new_node = new Node(new_data);
+
+                /* 3. Make next of new Node as head */
+                new_node.next = head;
+
+                /* 4. Move the head to point to new Node */
+                head = new_node;
+            }
+
+            /* Returns count of nodes in linked list */
+            public int getCount()
+            {
+                Node temp = head;
+                int count = 0;
+                while (temp != null)
+                {
+                    count++;
+                    temp = temp.next;
+                }
+                return count;
+            }
+
+            /* Driver program to test above functions. Ideally
+            this function should be in a separate user class.
+            It is kept here to keep code compact */
+
+        }
+    }
+    }
